3891d86525d392b85d432df30be63d4b8e2cf214
yaretzi.ortiz <ortizyarezti@gmail.com>
2024-09-12 11:15:07 -0600

    02 Variables let y const

---
+++ concepts/variables.js
@@ -0,0 +1,20 @@
+// Variables en Javascripts
+// Declaramos variables con let o const
+// let para variables que cambiaran de valor
+// const para variables que no cambiaran de valor 
+
+let name;
+console.log(name); 
+name = "Peter";
+console.log(name);
+console.log(typeof name);
+name = 18;
+console.log(name);
+console.log(typeof name);
+
+const PI = 3.1416;
+console.log(PI);
+console.log(typeof PI);
+//PI = 3.15; // Error
+
+
