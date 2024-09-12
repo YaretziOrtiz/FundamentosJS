347e610ea10a6ebad8af8f1e0c1994226b7f0d4c
yaretzi.ortiz <ortizyarezti@gmail.com>
2024-09-12 11:29:57 -0600

    03 Operadores

---
+++ concepts/operators.js
@@ -0,0 +1,41 @@
+// Operadores Matematematicos
+// + y -
+// * y /
+// % y ** Modulo y potencia
+
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3);
+
+
+// Operadores string 
+// concatenacion con +
+console.log("Hola" + " " + "Mundo");
+
+// Operadores de asignacion
+let a = 1;
+a += 3;
+a -= 3;
+a *= 3;
+a /= 3;
+a %= 3;
+a **= 3;
+console.log(a); 
+
+// Operadores de comparacion
+console.log(10 > 3);
+console.log(10 < 3);
+console.log(10 >= 3);
+console.log(10 <= 3);
+console.log(10 == 3);
+console.log(10 === 3);
+console.log(10 != 3);
+console.log(10 !== 3);
+
+// Operadores logicos
+console.log(true && true); // And
+console.log(true || true); // Or
+console.log(!true); // Not
